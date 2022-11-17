e9de34d10faf6eaf746500e78cfa82ed50c11295
dev-welligtonlins <welligtonlinsemp@gmail.com>
2022-11-17 18:11:22 -0500

    acessando os links pelo script.js(incompleto)

---
+++ project-favorites/public/js/script.js
@@ -0,0 +1,19 @@
+
+
+const loadingElement = document.querySelector('#loading')
+const favoritesContainer = document.querySelector('#links')
+
+
+
+console.log(favoritesContainer)
+
+
+function getAddFavorites(comment) {
+    
+    
+    const li = document.createElement('li')
+   
+    favoritesContainer.appendChild(li)
+}
+
+getAddFavorites('frfrfrf')
